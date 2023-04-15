af394e54295e6f89ffaef2e9fc35e5b3e2f3c37b
JohnNixon6972 <johnbrightnixon@gmail.com>
2023-04-15 10:46:00 +0530

    Added api code

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import  express from 'express';
 import { Actor,HttpAgent } from '@dfinity/agent';
 import {Principal } from '@dfinity/principal';
+import { idlFactory } from './account/Account.did.js';
+
 
 const app = express();
 
@@ -10,28 +12,29 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-import { idlFactory } from './account/Account.did.js';
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
     });
 
-    
+
 app.post('/api/account', async (req, res) => {
         
         async function CreateAccount(name, email) {
-            const agent = new HttpAgent({ host: 'http://localhost:8005' });
-            const accountActor =await Actor.createActor(idlFactory, { agent, canisterId: Principal.fromText('ryjl3-tyaaa-aaaaa-aaaba-cai') });
+            const agent = new HttpAgent({ host: 'http://127.0.0.1:8000' });
+            // log(agent);
+            const accountActor =await Actor.createActor(idlFactory, { agent, canisterId: Principal.fromText('rrkah-fqaaa-aaaaa-aaaaq-cai') });
             const result =await  accountActor.create_account(String(name), String(email));
             console.log(result);
+            return
          }
   
         var name = req.body.name;
         var email = req.body.email;
 
         await CreateAccount(name, email);
-      
+        res.send('Account created');
    
     }
 );
