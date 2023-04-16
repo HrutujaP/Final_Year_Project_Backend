59a26a36c2419ae359a60323cb89cbba50f62f1b
John Nixon <johnbrightnixon@gmail.com>
2023-04-16 20:54:20 +0530

    sucessfully connected backend with frontend

--- api/agent.js
+++ api/agent.js
@@ -1,6 +1,18 @@
 import { identity } from './identity.js';
-import fetch from 'node-fetch';
+import { fetch,Headers } from 'cross-fetch';
 import { HttpAgent } from '@dfinity/agent';
+
+if(!globalThis.fetch) {
+    globalThis.fetch = fetch;
+    globalThis.Headers = fetch.Headers;
+}
+
+if(!global.fetch){
+    global.fetch = fetch;
+    global.Headers = fetch.Headers;
+}
+
+new Headers();
 
 // const agent = new HttpAgent({identity : identity, host: 'http://127.0.0.1:4943',fetch: fetch });
 const agent = new HttpAgent({identity : identity, host: 'https://a4gq6-oaaaa-aaaab-qaa4q-cai.raw.ic0.app/',fetch: fetch });
